6d4f8ab87b1cac3f2729c6519f11208c3a1d5152
Maryam Ali <mali62541@gmail.com>
2016-12-13 22:39:09 -0500

    4.1 re-ran scaffolding using term 'text' instead of 'test' (--force) and ran rails db:migrate

--- db/migrate/20161214033411_create_products.rb
+++ db/migrate/20161214033411_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.test :description
+      t.text :description
       t.string :image_url
 
       t.timestamps
