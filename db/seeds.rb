4d9146f5f75edb8dfdae9a0ced5a3ef5c221c799
Maryam Ali <mali62541@gmail.com>
2016-12-17 01:34:31 -0500

    4.2 added product via db/seeds.rb generated migration to add color to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(:name => "seed-generated product", :description => "This was created from db/seeds.rb without using the HTML form")
